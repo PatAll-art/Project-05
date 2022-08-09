9035e4f11c431a8d1758b2edec76d04a7427e148
Patricia <patricia.allande@gmail.com>
2022-08-09 14:54:14 -0400

    added javascript and products

---
+++ front/js/script.js
@@ -0,0 +1,52 @@
+fetch('http://localhost:3000/api/products')
+    .then(data => {
+        return data.json();
+        }).then(json => AddProducts(json));
+
+    function AddProducts (products) {
+
+        let item = document.getElementById('items');
+        
+        for (let product of products) {
+            console.log(product.name);
+
+
+            let newArticle = document.createElement('article');
+            let newColors = document.createElement('p');
+            let newName = document.createElement('h3');
+            let newId = document.createElement('p');
+            let newPrice = document.createElement('p');
+            let newImg = document.createElement('img');
+            let newDescription = document.createElement('p');
+            let newAltTxt = document.createElement('p');
+            
+
+            newColors.textContent = product.colors;
+            newName.textContent = product.name;
+            newId.textContent = product._id;
+            newPrice.textContent = product.price;
+            newImg.src = product.imageUrl;
+            newDescription.textContent = product.description;
+            newAltTxt.textContent = product.altTxt;
+
+            newArticle.appendChild(newColors);
+            newArticle.appendChild(newName);
+            newArticle.appendChild(newId);
+            newArticle.appendChild(newPrice);
+            newArticle.appendChild(newImg);
+            newArticle.appendChild(newDescription);
+            newImg.appendChild(newAltTxt);
+
+            newName.classList.add('productName');
+            newDescription.classList.add('productDescription');
+
+            const sectionProduct = document.querySelector('section');
+            
+            sectionProduct.appendChild(newArticle);
+
+            
+        }
+            
+    
+
+    }
