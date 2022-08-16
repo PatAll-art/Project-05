d02ad59c3f47831ac649f097e903c96bacec3bbf
Patricia <patricia.allande@gmail.com>
2022-08-16 19:58:22 -0400

    added color drop menu and img

--- front/js/product.js
+++ front/js/product.js
@@ -21,25 +21,44 @@ for (i = 0; i < products.length; i++) {
         const title = document.getElementById('title');
         const price = document.getElementById('price');
         const description = document.getElementById('description');
-        const color = document.getElementsByClassName('item__content__settings__color');
-
-        const imgCont = document.getElementsByClassName('item__img');
-        const itemImg = document.createElement('img');
-        itemImg.src = products[i].imageUrl;
-
-
+      
         title.textContent = products[i].name;
         price.textContent = products[i].price;
         description.textContent = products[i].description;
-        color.textContent = products[i].colors;
-    
+  
+        const imgCont = document.getElementsByClassName('item__img');
+        const itemImg = document.createElement('img');
 
-        imgCont.appendChild(itemImg);
+        itemImg.src = products[i].imageUrl;
 
-    }
+        imgCont[0].appendChild(itemImg);
+
+        const colors = products[i].colors;
+
+
+       for (j = 0; j < colors.length; j++) {
+
+
+            const color = colors[j]
+
+        console.log(color);
+
+            const options = document.getElementById('colors');
+
+            const colorOp = document.createElement('option');
+
+            colorOp.textContent = color;
+
+            colorOp.setAttribute('value', color);
+
+            options.appendChild(colorOp);
+       
+
+
+       }
    
-    
 
 }
 
+}
 }
